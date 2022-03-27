9ccd88cb04fedbd3f64b2f208b6916431ae1e02b
hkursatsimsek <kursat.simsek@modanisa.com>
2022-03-27 19:05:25 +0300

    Added ArticleTableViewCell design and configured ArticleTableView DataSource and delegate.

--- breaking-news/Controllers/ArticleTableViewCell.swift
+++ breaking-news/Controllers/ArticleTableViewCell.swift
@@ -8,16 +8,24 @@
 import UIKit
 
 class ArticleTableViewCell: UITableViewCell {
+    @IBOutlet weak var articleImageView: UIImageView!
+    @IBOutlet weak var articleTitleLabel: UILabel!
+    @IBOutlet weak var articleAuthorLabel: UILabel!
+    @IBOutlet weak var articlePublishedLabel: UILabel!
+    @IBOutlet weak var articleSourceLabel: UILabel!
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    func setArticle(with article : Article) {
+        if let title = article.title, let author = article.author, let published = article.publishedAt, let source = article.source.name {
+            print("\(title) * \(author) * \(published) * \(source)")
+            articleTitleLabel.text = title
+            articleAuthorLabel.text = author
+            articlePublishedLabel.text = published
+            articleSourceLabel.text = source
+//            articleImageView.image = UIImage()
+        }
     }
-    
 }
--- breaking-news/Controllers/SearchArticleViewController.swift
+++ breaking-news/Controllers/SearchArticleViewController.swift
@@ -9,12 +9,24 @@ import UIKit
 import Moya
 
 class SearchArticleViewController: UIViewController {
+    @IBOutlet weak var articleSearchBar: UISearchBar!
+    @IBOutlet weak var articleFromTextField: UITextField!
+    @IBOutlet weak var articleToTextField: UILabel!
+    @IBOutlet weak var articleShortByTextField: UITextField!
+    @IBOutlet weak var articleTableView: UITableView!
     
     let networkManager = NewsNetworkManager()
     var articleList = [Article]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        articleTableView.delegate = self
+        articleTableView.dataSource = self
+        articleSearchBar.delegate = self
+
+        // Register TableViewCell
+        self.articleTableView.register(ArticleTableViewCell.self, forCellReuseIdentifier: "articleTableViewCellID")
 
         fetchArticlesGeneric()
 //        fetchArticles()
@@ -25,14 +37,16 @@ class SearchArticleViewController: UIViewController {
 // MARK: Methods
 extension SearchArticleViewController {
     func fetchArticlesGeneric() {
-        networkManager.fetchArticlesGeneric(searchText: "Intel") { [weak self] result in
+        networkManager.fetchArticlesGeneric(searchText: "Apple") { [weak self] result in
             guard let strongSelf = self else { return }
             switch result {
             case .success(let articleResponse):
                 strongSelf.articleList = articleResponse.articles
-                for article in strongSelf.articleList {
-                    print("\(article.author) - \(article.title)")
-                }
+                strongSelf.articleTableView.reloadData()
+//                for article in strongSelf.articleList {
+//                    print("\(article.author) - \(article.title) - \(article.source?.name) - \(article.publishedAt)")
+//                    print("-------")
+//                }
             case .failure(let error):
                 print(String(describing: error))
             }
@@ -41,14 +55,35 @@ extension SearchArticleViewController {
     
     func fetchArticles() {
         networkManager.fetchArticles(searchText: "Google") { articles in
-            
             self.articleList = articles
-            
-            for article in articles {
-                print(article.author)
-            }
+//            for article in articles {
+//                print(article.author)
+//            }
         }
     }
 
 }
 
+// MARK: TableView Delegate&DataSource
+extension SearchArticleViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        articleList.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "articleTableViewCellID", for: indexPath) as! ArticleTableViewCell
+        cell.selectionStyle = UITableViewCell.SelectionStyle.none
+        let article = self.articleList[indexPath.row]
+        cell.setArticle(with : article)
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200
+    }
+}
+
+// MARK: SearchBar
+extension SearchArticleViewController: UISearchBarDelegate {
+
+}
